c2eee4ceb45e55c213291043aed4c04288569e50
Natalie Burgei <natalieburgei04@gmail.com>
2023-10-04 15:00:45 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const divide =function(num1, num2){
+return (2000/100);
+}
+
+const square =(param1 , param2)=> {
+    return(param1 * param1)
+}
+square()
+const add =(f1 , f2)=> {
+return(4+3)}
